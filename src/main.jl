26244d42a4d50fda3984fbdab1a673ec1b04511c
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-07 00:58:07 +0100

    ...

--- src/main.jl
+++ src/main.jl
@@ -1,1 +1,14 @@
 #ICI JE VOUDRAIS FAIRE UNE BOUCLE NON ITERATIVE A L'EXEMPLE D'UNE EX2CUTION SHELL
+
+function main()
+    path = "data/street-map/Berlin_O_256.map"   # ou un chemin absolu pour tester
+
+    elts = charger_valider(path)
+
+    println("Chargement réussi !")
+    println("Chemin : ", elts.path)
+    println("Contenu brut :")
+    println(elts.content)
+end
+
+main()
