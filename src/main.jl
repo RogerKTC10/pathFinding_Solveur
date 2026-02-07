d71c549111918606c309bfe290a9da1aae0edc1d
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-07 17:43:47 +0100

    ....

--- src/main.jl
+++ src/main.jl
@@ -3,5 +3,6 @@ include("Carte_Operation/Chargement.jl")
 function main()
     path = "data/street-map/Berlin_0_256.map"
     elts = charger_valider(path)
-    println("Chargement effectuer avec succes !", elts)
+    println("Chargement effectuer avec succes !\n", elts)
 end
+main()
