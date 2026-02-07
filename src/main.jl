2f3d65a6e2b5117b84070cd63d2d7c34baf24671
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-07 01:11:53 +0100

    ...

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,10 @@
 #ICI JE VOUDRAIS FAIRE UNE BOUCLE NON ITERATIVE A L'EXEMPLE D'UNE EX2CUTION SHELL
 include("Carte_Operation/Charger_Carte.jl")
 
-function main()
-    path = "street-map/Berlin_0_256.map"
-    elts = charger_valider(path)
-    println("Chargement OK !")
-end
-
+function main() 
+    path = joinpath(@__DIR__, "..", "street-map", "Berlin_0_256.map") 
+    elts = charger_valider(path) 
+    println("Chargement OK !") 
+end 
 main()
 
