22c3ec99ec08d1a2b083113455b96c6610fcc810
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-08 15:36:58 +0100

    ...

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,20 @@
 include("Carte_Operation/Chargement.jl")
+include("Carte_Operation/Transformation.jl")
+
 
 function main()
     path = "data/street-map/Berlin_0_256.map"
     elts = charger_valider(path)
-    println("Chargement effectuer avec succes !\n", elts)
+    if elts != nothing
+        println("Chargement effectuer avec succes !\n")
+    end
+    matrice = Remplir_Matrice_Cons(path)
+
+    if isnothing(matrice)
+        println("Erreur de creation")
+    else
+        println(size(matrice))
+        println(matrice)
+    end
 end
 main()
