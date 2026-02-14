44779231eaf3c7ca4473417d0ec247a5144c2339
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-14 22:52:32 +0100

    ...

--- src/My_Algorithms/Utils_Algorithms.jl
+++ src/My_Algorithms/Utils_Algorithms.jl
@@ -1,3 +1,15 @@
+function Dictionnaire_Action(Char)
+    dic = ['.', '@','T','S', 'W']
+    for i in dic
+        if i == dic[1] || i == dic[3]
+            message = "Zone de blocage, passage refusé"
+            return (message, false)
+        else
+            message = "Passage facile pour le BFS"
+           return (message,true)  
+        end
+    end
+end
 
 function Passage_decision()
 end
