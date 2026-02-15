ed6fac995dd5dbbf0562734370790975f49c1064
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-15 22:39:05 +0100

    ...

--- src/affichage.jl
+++ src/affichage.jl
@@ -1,12 +1,9 @@
 import Pkg
-
 Pkg.add("Plots")
-
 using Plots
+
 include("Security_Transformation/Structure.jl")
-
 using .Struct_Carte
-
 
 function affichage_visible(carte, depart, arriver, chemin, type_Parcours)
     if (type_Parcours == "BFS")
