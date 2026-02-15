db70469e4ac05af26ccbe07afaa06275c171d94c
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-15 03:06:08 +0100

    ...

--- src/My_Algorithms/BFS_Doc/BFS.jl
+++ src/My_Algorithms/BFS_Doc/BFS.jl
@@ -1,19 +1,42 @@
 using .Struct_Carte
 
-function algo_BFS(carte::Carte_final_Struct, pt_depart, pt_arriver)
-    file = [pt_depart]
-    visite = Set[pt_arriver]
-    dic_stock = Dict()
-    value = 0
-    vue = false
-
-    while(!isempty(file))
-        actuel = shift!(file)
-        nb_etats_evalues += 1 
-        if actuel == goal 
-            vue = true 
-            break 
-        end #je laisse ca en suspend ... Je continuerai demain
+function execution_BFS(carte::Carte_final_Struct, depart, arriver)
+    file = [depart]                 
+    visites = Set([depart])               
+    parents = Dict()
+    nb_etats_evalues = 0
+    trouve = false
+    while !isempty(file)
+        (i, j) = popfirst!(file)
+        nb_etats_evalues += 1
+        if ((i, j) == arriver)
+            trouve = true
+            break
+        end
+        for voisin in Voisinage(i, j)
+            (ni, nj) = voisin   
+            if (ni >= 1 && ni <= carte.height && nj >= 1 && nj <= carte.width)
+                caractere_case = carte.grille[ni, nj]
+                
+                message, autorise = BFS_dic_Action(caractere_case)
+                
+                if autorise
+                    if !(voisin in visites)
+                        push!(visites, voisin)
+                        
+                        parents[voisin] = (i, j) 
+                        push!(file, voisin)
+                    end
+                end
+            end
+        end
     end
 
+    if trouve
+        chemin_final = reconstruire_chemin(parents, depart, arriver)
+        return (chemin = chemin_final, activite = nb_etats_evalues, distance = length(chemin_final)-1)
+    else
+        println("Pas de chemin trouvé.")
+        return (chemin = [], activite = nb_etats_evalues, distance = -1)
+    end
 end
