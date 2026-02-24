d77f94c3aacad9b036f2f4eefeb8b7c627b78dae
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-24 12:44:50 +0100

    DJISKTRA IMPLEMENTATION TERMINER

--- src/Comparaison.jl
+++ src/Comparaison.jl
@@ -3,17 +3,18 @@ include("My_Algorithms/Utils_Algorithms.jl")
 
 #---Spécifiquement pour le BFS------
 function calculer_cout_chemin(chemin, matriceV)
-    if isempty(chemin)
-        return Inf
-    end
+    total_facture = 0.0
 
-    total = 0.0
-    # On commence à l'indice 2 pour ignorer le coût de la case de départ
+    # On parcourt le chemin à partir de la deuxième case
     for k in 2:length(chemin)
         i, j = chemin[k]
-        total += matriceV[i, j]
+        caractere = matriceChar[i, j]
+        prix_passage = valuation(caractere)
+        
+        total_facture += prix_passage
     end
-    return total
+
+    return total_facture
 end
 
 function CPUtime()
