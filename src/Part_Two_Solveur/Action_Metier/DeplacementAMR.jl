9f07890933baa12b4214889ec7590e4f6601e85e
RogerKTC10 <ntchamroger82@gmail.com>
2026-03-26 15:57:44 +0100

    Evolution de la phase 2 du projet

---
+++ src/Part_Two_Solveur/Action_Metier/DeplacementAMR.jl
@@ -0,0 +1,17 @@
+include("AMR.jl")
+include("Entrepot_Tri.jl")
+include("Fournisseur&Client.jl")
+include("../Adaptation/Structure_Part2.jl")
+
+
+function plan_de_Mission()
+end
+
+function Acutaliser_le_chrono()
+end
+
+function Reservation()
+end
+
+function Gestion_des_Accidents()
+end
